4509403974fb557cb79b7b531f0f4c8f1e03a24e
Vaibhaw Raj <vaibhawraj@yahoo.in>
2017-05-01 00:05:15 +0530

    Polling Blog Update

--- FetchUpdate.py
+++ FetchUpdate.py
@@ -3,6 +3,8 @@
 import sys
 import requests
 import DataStorageHandler
+import datetime
+from dateutil.parser import parse as parseDate
 import xml.etree.ElementTree as ET
 #Get List of Blogs
 #Start Polling
@@ -37,28 +39,29 @@ def poll(blogRec):
 
 def parseXML(pollRes):
 	#parseXML will parse pollRes XML and return dictionary with blog Title, Link, List of Items
-	retDict = {'title':None,'link':None,'description':None,'items':None}
+	retDict = {'items':None, 'blogRec':pollRes['blogRec']}
 	if pollRes['status'] == True:
 		blogRec = pollRes['blogRec']
 		if pollRes['xmlBody'] != None:
 			#Start Parsing
 			try:
-				root = ET.fromString(pollRes['xmlBody'])
+				root = ET.fromstring(pollRes['xmlBody'])
+				
 				#Need to identify XML Structure
 				#For RSS, it must be of Standard structure with channel & items
 				channelNode = root.find('channel')
 				if channelNode != None:
-					titleNode = channel.find('title')
-					linkNode = channel.find('link')
-					descriptionNode = channel.find('description')
-					itemNodeList = channel.findAll('item')
+					titleNode = channelNode.find('title')
+					linkNode = channelNode.find('link')
+					descriptionNode = channelNode.find('description')
+					itemNodeList = channelNode.findall('item')
 					
 					if titleNode != None:
-						retDict['title'] = titleNode.text
+						retDict['blogRec']['title'] = titleNode.text
 					if linkNode != None:
-						retDict['link'] = linkNode.text
+						retDict['blogRec']['link'] = linkNode.text
 					if descriptionNode != None:
-						retDict['description'] = descriptionNode.text
+						retDict['blogRec']['description'] = descriptionNode.text
 
 					items = []
 					for itemNode in itemNodeList:
@@ -66,9 +69,16 @@ def parseXML(pollRes):
 						itemLinkNode = itemNode.find('link')
 						itemPubNode = itemNode.find('pubDate')
 						
-						print(itemPubNode.text)
 						item = {'title':None, 'link':None, 'pubDate':None}
-
+						if itemTitleNode != None:
+							item['title'] = itemTitleNode.text
+						if itemLinkNode != None:
+							item['link'] = itemLinkNode.text
+						if itemPubNode != None:
+							item['pubDate'] = itemPubNode.text
+						items.append(item)
+					retDict['items'] = items
+					blogRec['status'] = True
 				else:
 					blogRec['errorMessage'] = 'Improper RSS Structure'
 					blogRec['delete'] = True
@@ -77,7 +87,55 @@ def parseXML(pollRes):
 				blogRec['errorCount'] = blogRec['errorCount'] + 1
 				if blogRec['errorCount'] > 10:
 					blogRec['delete'] = True
-			
-blogRec = {'url':'http://www.google.com','errorCount':0}
-pollRes = poll(blogRec)
-print(blogRec)
+		retDict['blogRec'] = blogRec
+	return retDict
+
+def pollAll(blogRecList,itemRecList):
+	for blogRec in blogRecList:
+		if blogRec['delete'] == False:
+			pollRes = poll(blogRec)
+			parseRes = parseXML(pollRes)
+			if parseRes['blogRec']['status'] == True:
+				blogRec = parseRes['blogRec']
+				firstTime = blogRec['firstTime']
+				items = parseRes['items']
+				for item in items:
+					if item['pubDate'] != None:
+						pDate = parseDate(item['pubDate'])
+						if firstTime or pDate.date() == datetime.today():
+							newItemRec = {}
+							newItemRec['title'] = item['title']
+							newItemRec['pubDate'] = item['pubDate']
+							newItemRec['link'] = item['link']
+							newItemRec['blogUrl'] = blogRec['url']
+							newItemRec['isNew'] = True
+							isExist = False
+							for itemRec in itemRecList:
+								if itemRec['link'] == newItemRec['link']:
+									isExist = True
+									break
+							if isExist == False:
+								itemRecList.append(newItemRec)
+				if firstTime:
+					blogRec['firstTime'] = False
+						
+	print(blogRecList)
+	print(itemRecList)
+
+def newBlogRec(url):
+	blogRec = {'url':url,'errorCount':0,'status':False,'firstTime':True,'delete':False}
+	return blogRec
+
+def fetchAllBlogRec():
+	pass
+
+def fetchAllItemRec():
+	pass
+
+def save
+blogRecList = []
+blogRec = {'url':'http://blog.smola.org/rss','errorCount':0,'status':False,'firstTime':True,'delete':False}
+blogRecList.append(blogRec)
+itemRecList = []
+pollAll(blogRecList,itemRecList)
+
